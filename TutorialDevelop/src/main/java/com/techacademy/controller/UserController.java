40a888bffc583efc641914df874383606a3d939f
dir-FY24fujiiyuzuki <yuzuki.fujii.p795@dirbato.co.jp>
2024-04-25 15:56:20 +0900

    l21-4~5

---
+++ TutorialDevelop/src/main/java/com/techacademy/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.techacademy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam; 
+import java.util.Set;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techacademy.service.UserService;
+
+import org.springframework.web.bind.annotation.ModelAttribute; // 追加
+import org.springframework.web.bind.annotation.PostMapping; 
+import com.techacademy.entity.User; 
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    /** 一覧画面を表示 */
+    @GetMapping("/list")
+    public String getList(Model model) {
+        // 全件検索結果をModelに登録
+        model.addAttribute("userlist", service.getUserList());
+        // user/list.htmlに画面遷移
+        return "user/list";
+    }
+    // ----- 追加:ここから -----
+    /** User登録画面を表示 */
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        // User登録画面に遷移
+        return "user/register";
+    }
+
+    /** User登録処理 */
+    @PostMapping("/register")
+    public String postRegister(User user) {
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    /** User削除処理 */
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+        // Userを一括削除
+        service.deleteUser(idck);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+}
---
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -0,0 +1,37 @@
+package com.techacademy.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.techacademy.entity.User;
+import com.techacademy.repository.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Set; 
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    /** 全件を検索して返す */
+    public List<User> getUserList() {
+        // リポジトリのfindAllメソッドを呼び出す
+        return userRepository.findAll();
+    }
+    
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    // ----- 追加:ここから -----
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
+    }
+    
+}
